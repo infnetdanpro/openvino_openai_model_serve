36967a208cf0d1b1b30a52d701bef340ee747626
infnetdanpro <33713163+infnetdanpro@users.noreply.github.com>
2026-01-15 11:30:25 +0100

    Update config - require models

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from openvino_genai import LLMPipeline, VLMPipeline
+# from openvino_genai import LLMPipeline, VLMPipeline
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,13 +7,15 @@ class Config(BaseSettings):
     HTTP_PORT: int = 11434
 
     DEVICE: str = "CPU"  # e.g., "CPU", "NPU", "GPU.0"
-
     MODELS_DIR: str = "models"
     # {"model_id": (model_name, pipeline_class)}
-    MODELS: dict = {
-        "gemma": ("gemma-3-4b-it-int4-cw-ov", VLMPipeline),
-        "mistral": ("mistral-7b-instruct-v0.1-int4-ov", LLMPipeline),
-    }
+    # Example
+    # MODELS: dict = {
+    #     "gemma": ("gemma-3-4b-it-int4-cw-ov", VLMPipeline),
+    #     "mistral": ("mistral-7b-instruct-v0.1-int4-ov", LLMPipeline),
+    # }
+    # You must download models, see README for more details.
+    MODELS: dict = {}
 
     model_config = SettingsConfigDict(env_file=".env")
 
